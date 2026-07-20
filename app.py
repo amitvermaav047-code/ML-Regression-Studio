1b2532e837f5fbbb2f8cc7c33a2b7783e3e5d4c2
Amit Verma <amitverma.av047@gmail.com>
2026-07-21 02:44:41 +0530

    Updated encoding and reset feature

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-import streamlit as st
 import numpy as np 
 import pandas as pd
+import streamlit as st
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score , mean_absolute_error , mean_squared_error , root_mean_squared_error
 #--------------- PAGE CONFIG--------------------
 st.set_page_config(
@@ -54,8 +55,10 @@ with tab1:
             "file_name" not in st.session_state
             or st.session_state.file_name!=file.name
         ):
+            file.seek(0)
             st.session_state.file_name=file.name
             st.session_state.df=pd.read_csv(file)
+            st.session_state.original_df = st.session_state.df.copy()
         st.success("✅ CSV file uploaded successfully!")
         df = st.session_state.df
         st.header("Datasets 📁")
@@ -178,6 +181,42 @@ with tab2:
             st.session_state.df=df.drop_duplicates()
             st.session_state.R_duplicate=True
             st.rerun()
+    st.divider()
+    cat_col = df.select_dtypes(include="object").columns
+    if len(cat_col) == 0 :
+        st.info("No Categorical Columns available")
+    else:
+        st.subheader("Categorical Encoding")
+        method = st.selectbox(
+            "Select Encoding Method",
+            ["~~Select an option~~","Label Encoding","One Hot Encoding"]
+        )
+        if st.button("Apply Encoding"):
+            if method!="~~Select an option~~":
+                if method=="Label Encoding":
+                    le = LabelEncoder()
+                    for col in cat_col:
+                        df[col] = le.fit_transform(df[col])
+                    st.session_state.df = df
+                    st.success("Label Encoding Applied Successfully")
+                    st.rerun()
+                elif method == "One Hot Encoding":
+                    df = pd.get_dummies(
+                        df,
+                        columns=cat_col,
+                        drop_first=True,
+                        dtype=int
+                    )
+                    st.session_state.df = df
+                    st.success("One Hot Encoding Applied Successfully!")
+                    st.rerun()
+    st.divider()
+    st.subheader("Reset Data")
+    if st.button("Reset Data",use_container_width=True):
+        st.session_state.df = st.session_state.original_df.copy()
+        st.success("Reset data Successfully")
+        st.rerun()
+
        
 #---------------------------TAB-3----------------------------------
 with tab3:
@@ -185,6 +224,7 @@ with tab3:
         st.warning("Please upload dataset first.")
         st.stop()
     df = st.session_state.df
+    numeric_col = df.select_dtypes(include="number").columns.tolist()
     st.header("Feature Selection 🎯")
     st.subheader("Select Model")
     model = st.selectbox(
@@ -194,29 +234,29 @@ with tab3:
     if model=="Linear Regression":
         Ind = st.selectbox(
             "Select Independent Column",
-            ["~~Select an Column~~"] + list(df.columns)
+            ["~~Select an Column~~"] + numeric_col
         )
         dep = st.selectbox(
             "Select Dependent Column",
-            ["~~Select an Column~~"] + list(df.columns)
+            ["~~Select an Column~~"] + numeric_col
         )
     elif model=="Multiple Linear Regression":
         Ind = st.multiselect(
             "Select Independent Columns",
-            list(df.columns)
+            numeric_col
         )
         dep = st.selectbox(
             "Select Dependent Column",
-            ["~~Select an Column~~"] + list(df.columns)
+            ["~~Select an Column~~"] + numeric_col
         )
     elif model=="polynomial Regression":
         Ind = st.selectbox(
             "Select Independent Column",
-            ["~~Select an Column~~"] + list(df.columns)
+            ["~~Select an Column~~"] + numeric_col
         )
         dep = st.selectbox(
             "Select Dependent Column",
-            ["~~Select an Column~~"] + list(df.columns)
+            ["~~Select an Column~~"] + numeric_col
         )
         degree = st.slider(
             "Select Polynomial Degree",
@@ -254,6 +294,7 @@ with tab4:
     Dep = st.session_state.dep
     if selected_model=="polynomial Regression":
         degree = st.session_state.degree
+
     st.header("Model Training 🤖")
     st.subheader("Selected Configuration")
     col1 , col2 = st.columns(2)
@@ -269,8 +310,19 @@ with tab4:
     if selected_model == "polynomial Regression":
         st.metric("Polynomial Degree", st.session_state.degree)
     st.divider()
-
+    
+    if df.isnull().sum().sum()>0:
+        st.error("Please clean data first")
+        st.stop()
     if st.button("Train Model",use_container_width=True):
+        if selected_model == "Linear Regression" or selected_model =="polynomial Regression":    
+            if Ind == Dep:
+                st.error("Independent and Dependent columns cannot be same.")
+                st.stop()
+        elif selected_model=="Multiple Linear Regression":
+            if Dep in Ind:
+                st.error("Dependent column can't be selected as Independent")
+                st.stop()
         with st.spinner("Trainning Model..."):
             if selected_model=="Linear Regression":
                 X = df[[Ind]]
